89f64c979da089c9bf8e15d9644d00dc39296f06
Shawn Choi <schoi@godaddy.com>
2020-11-03 23:22:41 -0800

    Abstract out main from execute entrypoint

--- docker/execute.py
+++ docker/execute.py
@@ -13,12 +13,10 @@ from .errors import SecretManagerRetrievalError, ExitContainerScanner
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-CIRRUS_SCAN_BUCKET_ENV_NAME = "CIRRUS_SCAN_RESULTS_BUCKET"
-
 
 def get_audit_role_arn():
     """Get audit role ARN based on an account bucket"""
-    accounts_bucket = os.getenv(CIRRUS_SCAN_BUCKET_ENV_NAME)
+    accounts_bucket = os.getenv("CIRRUS_SCAN_RESULTS_BUCKET")
 
     if not accounts_bucket:
         logger.error(
@@ -43,6 +41,89 @@ def get_audit_role_arn():
     return auditRoleArn
 
 
+def main():
+    """Main entry point for ContainerScanner logic"""
+    results = None
+    region = boto3.session.Session().region_name
+    audit_role_arn = get_audit_role_arn()
+
+    sts_client = SecurityTokenServiceClient(logger)
+    sm_client = SecretsManagerClient(logger)
+    ssm_client = SSMClient(logger)
+    ecr_client = ECRClient(logger)
+    ec2_client = EC2Client(logger)
+    s3_client = S3Client(logger)
+
+    vpc_id = wrapper.get_parameters()["vpc_id"]
+    task_uuid = os.getenv("CIRRUS_SCAN_TASK_UUID", "UNDEFINED")
+    task_name = f"/CirrusScan/containerscan/{vpc_id}/{task_uuid}"
+    isProvisioned = ssm_client.has_task_parameter(task_name)
+
+    if not isProvisioned and ecr_client.has_repositories(region):
+        # launch EC2 through service catalog with user data
+        # - register ECR registry in Prisma with hostname
+        # - force repo scan
+        # - poll repo scan progress
+        # - when complete, get repo scan details, use pagination
+        # generate findings for security hub
+        prisma_client = PrismaClient(logger, sts_client, sm_client, ec2_client)
+        prisma_scanner = Scanner(logger, s3_client)
+        prisma_token = prisma_client.get_token(audit_role_arn)
+
+        account_id = sts_client.get_account_id()
+        ecr_registry_name = f"{account_id}.dkr.ecr.{region}.amazonaws.com"
+        provisioned_product_name = f"ContainerScanner-{vpc_id}"
+
+        ssm_client.create_task_parameter(task_name)
+
+        prisma_scanner.provision_scanner(provisioned_product_name, vpc_id)
+
+        prisma_client.register_ecr_registry(
+            prisma_token, ecr_registry_name, vpc_id)
+
+        prisma_client.force_ecr_registry_scan(prisma_token, ecr_registry_name)
+
+        prisma_client.wait_for_scan_completion()
+
+        results = prisma_client.retrieve_scanner_results(
+            prisma_token, ecr_registry_name)
+
+        prisma_scanner.save_scanner_results(results)
+
+        security_hub_mgr = securityhub.SecurityHub_Manager(
+            exception_rules=wrapper.get_exception_rules())
+
+        security_hub_mgr.begin_transaction(
+            scope_prefix="containerscan/" + security_hub_mgr.aws_region(),
+            scope_region=security_hub_mgr.aws_region(),
+        )
+
+        if results is not None:
+            prisma_scanner.evaluate_scanner_results(
+                vpc_id, security_hub_mgr, results)
+
+        prisma_scanner.generate_informational_finding(security_hub_mgr)
+        security_hub_mgr.end_transaction(
+            autoarchive=True, dont_archive=None)
+
+        # Pass back finding demographic information. For purposes
+        # of this scanner, any finding with a normalized severity
+        # of at least 70 constitutes a compliance failure.
+        scan_info = security_hub_mgr.get_finding_data()
+        compliance = "PASS"
+        for severity in scan_info["severity"]:
+            if severity >= 70:
+                compliance = "FAIL"
+                break
+        wrapper.put_status(
+            {"status": "SUCCESS", "compliance": compliance,
+                "finding_data": scan_info}
+        )
+
+        prisma_scanner.deprovision_scanner(provisioned_product_name)
+        ssm_client.delete_task_parameter(task_name)
+
+
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s"
@@ -53,87 +134,7 @@ if __name__ == "__main__":
     logging.getLogger("urllib3").setLevel(logging.CRITICAL)
 
     try:
-        results = None
-        region = boto3.session.Session().region_name
-        audit_role_arn = get_audit_role_arn()
-
-        sts_client = SecurityTokenServiceClient(logger)
-        sm_client = SecretsManagerClient(logger)
-        ssm_client = SSMClient(logger)
-        ecr_client = ECRClient(logger)
-        ec2_client = EC2Client(logger)
-        s3_client = S3Client(logger)
-
-        vpc_id = wrapper.get_parameters()["vpc_id"]
-        task_uuid = os.getenv("CIRRUS_SCAN_TASK_UUID", "UNDEFINED")
-        task_name = f"/CirrusScan/containerscan/{vpc_id}/{task_uuid}"
-        isProvisioned = ssm_client.has_task_parameter(task_name)
-
-        if not isProvisioned and ecr_client.has_repositories(region):
-            # launch EC2 through service catalog with user data
-            # - register ECR registry in Prisma with hostname
-            # - force repo scan
-            # - poll repo scan progress
-            # - when complete, get repo scan details, use pagination
-            # generate findings for security hub
-            prisma_client = PrismaClient(
-                logger, sts_client, sm_client, ec2_client)
-            prisma_scanner = Scanner(logger, s3_client)
-            prisma_token = prisma_client.get_token(audit_role_arn)
-
-            account_id = sts_client.get_account_id()
-            ecr_registry_name = f"{account_id}.dkr.ecr.{region}.amazonaws.com"
-            provisioned_product_name = f"ContainerScanner-{vpc_id}"
-
-            ssm_client.create_task_parameter(task_name)
-
-            prisma_scanner.provision_scanner(provisioned_product_name, vpc_id)
-
-            prisma_client.register_ecr_registry(
-                prisma_token, ecr_registry_name, vpc_id)
-
-            prisma_client.force_ecr_registry_scan(
-                prisma_token, ecr_registry_name)
-
-            prisma_client.wait_for_scan_completion()
-
-            results = prisma_client.retrieve_scanner_results(
-                prisma_token, ecr_registry_name)
-
-            prisma_scanner.save_scanner_results(results)
-
-            security_hub_mgr = securityhub.SecurityHub_Manager(
-                exception_rules=wrapper.get_exception_rules())
-
-            security_hub_mgr.begin_transaction(
-                scope_prefix="containerscan/" + security_hub_mgr.aws_region(),
-                scope_region=security_hub_mgr.aws_region(),
-            )
-
-            if results is not None:
-                prisma_scanner.evaluate_scanner_results(
-                    vpc_id, security_hub_mgr, results)
-
-            prisma_scanner.generate_informational_finding(security_hub_mgr)
-            security_hub_mgr.end_transaction(
-                autoarchive=True, dont_archive=None)
-
-            # Pass back finding demographic information. For purposes
-            # of this scanner, any finding with a normalized severity
-            # of at least 70 constitutes a compliance failure.
-            scan_info = security_hub_mgr.get_finding_data()
-            compliance = "PASS"
-            for severity in scan_info["severity"]:
-                if severity >= 70:
-                    compliance = "FAIL"
-                    break
-            wrapper.put_status(
-                {"status": "SUCCESS", "compliance": compliance,
-                    "finding_data": scan_info}
-            )
-
-            prisma_scanner.deprovision_scanner(provisioned_product_name)
-            ssm_client.delete_task_parameter(task_name)
+        main()
     except ExitContainerScanner:
         logger.info("Exiting Container scanner!")
     except:
